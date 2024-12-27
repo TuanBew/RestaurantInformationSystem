98a2f87723d213485f07a5ac7dd7519fa64788c5
jhonny le <tuananhlenguyen9@gmail.com>
2024-12-27 11:54:46 +0700

    super up date

--- RestaurantSystem/ChefDashboard.Designer.cs
+++ RestaurantSystem/ChefDashboard.Designer.cs
@@ -55,7 +55,7 @@
             this.pnlSidebar.Controls.Add(this.pictureBox1);
             this.pnlSidebar.Dock = System.Windows.Forms.DockStyle.Left;
             this.pnlSidebar.Location = new System.Drawing.Point(0, 0);
-            this.pnlSidebar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pnlSidebar.Margin = new System.Windows.Forms.Padding(2);
             this.pnlSidebar.Name = "pnlSidebar";
             this.pnlSidebar.Size = new System.Drawing.Size(150, 630);
             this.pnlSidebar.TabIndex = 0;
@@ -74,7 +74,7 @@
             // 
             this.btnLogout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnLogout.Location = new System.Drawing.Point(38, 398);
-            this.btnLogout.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnLogout.Margin = new System.Windows.Forms.Padding(2);
             this.btnLogout.Name = "btnLogout";
             this.btnLogout.Size = new System.Drawing.Size(71, 25);
             this.btnLogout.TabIndex = 4;
@@ -86,7 +86,7 @@
             // 
             this.btnImportHistory.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnImportHistory.Location = new System.Drawing.Point(0, 305);
-            this.btnImportHistory.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnImportHistory.Margin = new System.Windows.Forms.Padding(2);
             this.btnImportHistory.Name = "btnImportHistory";
             this.btnImportHistory.Size = new System.Drawing.Size(150, 32);
             this.btnImportHistory.TabIndex = 3;
@@ -98,7 +98,7 @@
             // 
             this.btnInventory.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnInventory.Location = new System.Drawing.Point(0, 243);
-            this.btnInventory.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnInventory.Margin = new System.Windows.Forms.Padding(2);
             this.btnInventory.Name = "btnInventory";
             this.btnInventory.Size = new System.Drawing.Size(150, 32);
             this.btnInventory.TabIndex = 2;
@@ -110,7 +110,7 @@
             // 
             this.btnReceiveOrders.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnReceiveOrders.Location = new System.Drawing.Point(0, 173);
-            this.btnReceiveOrders.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnReceiveOrders.Margin = new System.Windows.Forms.Padding(2);
             this.btnReceiveOrders.Name = "btnReceiveOrders";
             this.btnReceiveOrders.Size = new System.Drawing.Size(150, 36);
             this.btnReceiveOrders.TabIndex = 1;
@@ -124,7 +124,7 @@
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(150, 75);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -137,16 +137,16 @@
             this.pnlMainContent.Controls.Add(this.flpTables);
             this.pnlMainContent.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pnlMainContent.Location = new System.Drawing.Point(150, 0);
-            this.pnlMainContent.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pnlMainContent.Margin = new System.Windows.Forms.Padding(2);
             this.pnlMainContent.Name = "pnlMainContent";
-            this.pnlMainContent.Size = new System.Drawing.Size(656, 630);
+            this.pnlMainContent.Size = new System.Drawing.Size(756, 630);
             this.pnlMainContent.TabIndex = 2;
             // 
             // pnlRightContent
             // 
             this.pnlRightContent.BackColor = System.Drawing.Color.Bisque;
             this.pnlRightContent.Dock = System.Windows.Forms.DockStyle.Right;
-            this.pnlRightContent.Location = new System.Drawing.Point(240, 0);
+            this.pnlRightContent.Location = new System.Drawing.Point(340, 0);
             this.pnlRightContent.Margin = new System.Windows.Forms.Padding(2);
             this.pnlRightContent.Name = "pnlRightContent";
             this.pnlRightContent.Size = new System.Drawing.Size(416, 630);
@@ -166,10 +166,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(806, 630);
+            this.ClientSize = new System.Drawing.Size(906, 630);
             this.Controls.Add(this.pnlMainContent);
             this.Controls.Add(this.pnlSidebar);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "ChefDashboard";
             this.Text = "ChefDashboard";
             this.Load += new System.EventHandler(this.ChefDashboard_Load);
---
+++ RestaurantSystem/RevenueControl.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace RestaurantSystem
+{
+    partial class RevenueControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.revenue_dataGridView = new System.Windows.Forms.DataGridView();
+            this.totalrevenue_groupbox = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.revenue_dataGridView)).BeginInit();
+            this.totalrevenue_groupbox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // revenue_dataGridView
+            // 
+            this.revenue_dataGridView.BackgroundColor = System.Drawing.Color.AntiqueWhite;
+            this.revenue_dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.revenue_dataGridView.Location = new System.Drawing.Point(22, 21);
+            this.revenue_dataGridView.Name = "revenue_dataGridView";
+            this.revenue_dataGridView.Size = new System.Drawing.Size(973, 494);
+            this.revenue_dataGridView.TabIndex = 0;
+            this.revenue_dataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.revenue_dataGridView_CellContentClick);
+            // 
+            // totalrevenue_groupbox
+            // 
+            this.totalrevenue_groupbox.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.totalrevenue_groupbox.Controls.Add(this.label1);
+            this.totalrevenue_groupbox.Location = new System.Drawing.Point(22, 540);
+            this.totalrevenue_groupbox.Name = "totalrevenue_groupbox";
+            this.totalrevenue_groupbox.Size = new System.Drawing.Size(973, 67);
+            this.totalrevenue_groupbox.TabIndex = 1;
+            this.totalrevenue_groupbox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(489, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // RevenueControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Bisque;
+            this.Controls.Add(this.totalrevenue_groupbox);
+            this.Controls.Add(this.revenue_dataGridView);
+            this.Name = "RevenueControl";
+            this.Size = new System.Drawing.Size(1020, 630);
+            ((System.ComponentModel.ISupportInitialize)(this.revenue_dataGridView)).EndInit();
+            this.totalrevenue_groupbox.ResumeLayout(false);
+            this.totalrevenue_groupbox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView revenue_dataGridView;
+        private System.Windows.Forms.GroupBox totalrevenue_groupbox;
+        private System.Windows.Forms.Label label1;
+    }
+}
